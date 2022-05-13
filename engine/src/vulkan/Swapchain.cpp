2c0561428dfffba27e5771a56ed7940493c50bee
unknown <davembertels@gmail.com>
2022-05-13 18:19:41 -0400

    Added Vulkan Swapchain class.

---
+++ engine/src/vulkan/Swapchain.cpp
@@ -0,0 +1,163 @@
+#include "Swapchain.h"
+#include "base.h"
+
+#include <algorithm>
+
+namespace sge::vulkan
+{
+	Swapchain::Swapchain(VkDevice device, VkSurfaceKHR surface, GLFWwindow* windowHandle,
+		SwapchainSupportDetails&& supportDetails, const QueueFamilyIndices& queueFamilyIndices)
+		: m_SwapchainHandle(nullptr), m_FramebufferResized(false)
+	{
+		//SwapchainSupportDetails details = QuerySwapchainSupport(instance->GetPhysicalDevice(), instance->GetSurface());
+
+		VkSurfaceFormatKHR surfaceFormat = ChooseSurfaceFormat(supportDetails.Formats);
+		VkPresentModeKHR presentMode = ChoosePresentMode(supportDetails.PresentModes);
+		VkExtent2D extent = ChooseExtent(supportDetails.Capabilities, windowHandle);
+
+		uint32_t imageCount = supportDetails.Capabilities.minImageCount + 1;
+		if (supportDetails.Capabilities.maxImageCount > 0 && imageCount > supportDetails.Capabilities.maxImageCount)
+			imageCount = supportDetails.Capabilities.maxImageCount;
+
+		VkSwapchainCreateInfoKHR createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		createInfo.surface = surface;
+		createInfo.imageFormat = surfaceFormat.format;
+		createInfo.imageColorSpace = surfaceFormat.colorSpace;
+		createInfo.presentMode = presentMode;
+		createInfo.imageExtent = extent;
+		createInfo.imageArrayLayers = 1;
+		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+		if (queueFamilyIndices.GraphicsFamily != queueFamilyIndices.PresentFamily)
+		{
+			createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+			createInfo.queueFamilyIndexCount = 2;
+		}
+		else
+			createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		createInfo.preTransform = supportDetails.Capabilities.currentTransform;
+		createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+		createInfo.clipped = VK_TRUE;
+		createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+		if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &m_SwapchainHandle) != VK_SUCCESS)
+			SGE_DEBUG_BREAKM("Failed to create Vulkan swap chain.");
+
+		vkGetSwapchainImagesKHR(device, m_SwapchainHandle, &imageCount, nullptr);
+		m_Images.resize(imageCount);
+		vkGetSwapchainImagesKHR(device, m_SwapchainHandle, &imageCount, m_Images.data());
+		m_ImageFormat = surfaceFormat.format;
+		m_Extent = extent;
+	}
+
+	void Swapchain::Destroy(VkDevice device)
+	{
+
+		for (auto framebuffer : m_Framebuffers)
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+
+		//m_Pipeline->Destroy(this);
+
+		//vkDestroyRenderPass(m_Device, m_RenderPass, nullptr);
+
+		vkDestroySwapchainKHR(device, m_SwapchainHandle, nullptr);
+
+		for (auto imageView : m_ImageViews)
+			vkDestroyImageView(device, imageView, nullptr);
+
+#ifdef DEBUG
+		m_CleanedUp = true;
+#endif
+	}
+
+	void Swapchain::InitImageViews(VkDevice device)
+	{
+		m_ImageViews.resize(m_Images.size());
+		for (size_t i = 0; i < m_Images.size(); i++)
+		{
+			VkImageViewCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = m_Images[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = m_ImageFormat;
+
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			if (vkCreateImageView(device, &createInfo, nullptr, &m_ImageViews[i]) != VK_SUCCESS)
+				SGE_DEBUG_BREAKM("Failed to create Vulkan image view.");
+		}
+	}
+
+	void Swapchain::InitFramebuffers(VkDevice device, VkRenderPass renderPass)
+	{
+		m_Framebuffers.resize(m_ImageViews.size());
+
+		for (size_t i = 0; i < m_ImageViews.size(); i++)
+		{
+			VkImageView attachment = m_ImageViews[i];
+
+			VkFramebufferCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			createInfo.renderPass = renderPass;
+			createInfo.attachmentCount = 1;
+			createInfo.pAttachments = &attachment;
+			createInfo.width = m_Extent.width;
+			createInfo.height = m_Extent.height;
+			createInfo.layers = 1;
+
+			if (vkCreateFramebuffer(device, &createInfo, nullptr, &m_Framebuffers[i]))
+				SGE_DEBUG_BREAKM("Failed to create Vulkan framebuffer.");
+		}
+	}
+
+
+	VkSurfaceFormatKHR Swapchain::ChooseSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats)
+	{
+		for (const auto& format : availableFormats)
+		{
+			if (format.format == VK_FORMAT_B8G8R8A8_SRGB && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+				return format;
+		}
+
+		return availableFormats[0];
+	}
+
+	VkPresentModeKHR Swapchain::ChoosePresentMode(const std::vector<VkPresentModeKHR>& availableModes)
+	{
+		for (const auto& mode : availableModes)
+		{
+			if (mode == VK_PRESENT_MODE_MAILBOX_KHR)
+				return mode;
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	VkExtent2D Swapchain::ChooseExtent(const VkSurfaceCapabilitiesKHR& capabilities, GLFWwindow* window)
+	{
+		if (capabilities.currentExtent.width != UINT_MAX)
+			return capabilities.currentExtent;
+
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		VkExtent2D actualExtent = {
+			static_cast<uint32_t>(width),
+			static_cast<uint32_t>(height)
+		};
+
+		actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+		return actualExtent;
+	}
+} // namespace sge::vulkan
---
+++ engine/src/vulkan/Swapchain.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Util.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vector>
+
+struct GLFWwindow;
+
+namespace sge::vulkan
+{
+	class Swapchain
+	{
+	private:
+		VkSwapchainKHR m_SwapchainHandle;
+		std::vector<VkImage> m_Images;
+		std::vector<VkImageView> m_ImageViews;
+		VkFormat m_ImageFormat;
+		VkExtent2D m_Extent;
+		std::vector<VkFramebuffer> m_Framebuffers;
+		bool m_FramebufferResized;
+#ifdef DEBUG
+		bool m_CleanedUp = false;
+#endif // DEBUG
+	public:
+		Swapchain(VkDevice device, VkSurfaceKHR surface, GLFWwindow* windowHandle,
+			SwapchainSupportDetails&& supportDetails, const QueueFamilyIndices& queueFamilyIndices);
+#ifdef DEBUG
+		~Swapchain()
+		{
+			SGE_ASSERTM(m_CleanedUp, "Vulkan swap chain not cleaned up.");
+		}
+#endif // DEBUG
+		void Destroy(VkDevice device);
+		void InitImageViews(VkDevice device);
+		void InitFramebuffers(VkDevice device, VkRenderPass renderPass);
+	public:
+		inline VkSwapchainKHR GetSwapchainHandle() const { return m_SwapchainHandle; }
+		inline bool GetFramebufferResized() const { return m_FramebufferResized; }
+		inline VkFormat GetImageFormat() const { return m_ImageFormat; }
+		inline VkExtent2D GetExtent() const { return m_Extent; }
+		inline VkFramebuffer FramebufferAt(uint32_t index) const { return m_Framebuffers[index]; }
+		inline void SetFramebufferResized(bool b) { m_FramebufferResized = b; }
+
+		VkSurfaceFormatKHR ChooseSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+		VkPresentModeKHR ChoosePresentMode(const std::vector<VkPresentModeKHR>& availableModes);
+		VkExtent2D ChooseExtent(const VkSurfaceCapabilitiesKHR& capabilities, GLFWwindow* window);
+	};
+} // namespace sge::vulkan
