949a1cbda9779bacac378b039b2c003ed3055b1c
unknown <davembertels@gmail.com>
2022-05-13 18:25:41 -0400

    Added 'debug break with message' macro.

--- engine/src/base.h
+++ engine/src/base.h
@@ -6,6 +6,7 @@
 	#define SGE_USING_VALIDATION_LAYERS
 
 	#define SGE_DEBUG_BREAK() __debugbreak()
-	#define SGE_ASSERTM(expr, msg) if (!(expr)) { std::cerr << "Assertion Failed: " << msg << '\n'; SGE_DEBUG_BREAK(); }
-	#define SGE_ASSERTF(expr, format, ...) if (!(expr)) { fprintf(stderr, format"\n", __VA_ARGS__); SGE_DEBUG_BREAK(); }
+	#define SGE_DEBUG_BREAKM(msg) { fprintf(stderr, "Assertion Failed: %s\n", msg); SGE_DEBUG_BREAK(); }
+	#define SGE_ASSERTM(expr, msg) if (!(expr)) SGE_DEBUG_BREAKM(msg)
+	#define SGE_ASSERTF(expr, format, ...) if (!(expr)) { fprintf(stderr, "Assertion Failed: "format"\n", __VA_ARGS__); SGE_DEBUG_BREAK(); }
 #endif // DEBUG
