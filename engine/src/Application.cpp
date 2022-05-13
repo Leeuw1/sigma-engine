30fff5ae83bbee998a207e99945fc9959f2f1557
unknown <davembertels@gmail.com>
2022-05-13 18:27:38 -0400

    Added 'OnEvent' function.

--- engine/src/Application.cpp
+++ engine/src/Application.cpp
@@ -1,9 +1,15 @@
 #include "Application.h"
+#include "event/KeyEvent.h"
+
+#include <iostream>
 
 namespace sge
 {
 	Application::Application()
 	{
+		m_Window.SetEventCallback(std::bind(Application::OnEvent_Static, this, std::placeholders::_1));
+		m_LayerStack.PushBack(new TestLayer("TEST LAYER 0"));
+		m_LayerStack.PushBack(new TestLayer("TEST LAYER 1"));
 	}
 
 	Application::~Application()
@@ -14,9 +20,30 @@ namespace sge
 	{
 		while (!m_Window.ShouldClose())
 		{
+			m_LayerStack.OnUpdate();
 			m_Window.OnUpdate();
 		}
 
 		return 0;
 	}
-}
+
+	void Application::OnEvent_Static(Application* app, Event& event)
+	{
+		app->OnEvent(event);
+	}
+
+	void Application::OnEvent(Event& event)
+	{
+		m_LayerStack.OnEvent(event);
+
+		switch (event.GetType())
+		{
+		case EventType::KeyPressed:
+		{
+			KeyPressedEvent& e = static_cast<KeyPressedEvent&>(event);
+			std::cout << "Application: Key pressed\n";
+			break;
+		}
+		}
+	}
+} // namespace sge
--- engine/src/Application.h
+++ engine/src/Application.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Window.h"
+#include "Layer.h"
+
+// Temporary
+#include <iostream>
 
 namespace sge
 {
@@ -8,10 +12,40 @@ namespace sge
 	{
 	private:
 		Window m_Window;
+		LayerStack m_LayerStack;
 	public:
 		Application();
 		~Application();
+		void OnEvent(Event& event);
+		static void OnEvent_Static(Application* app, Event& event);
 
 		int Run();
 	};
-}
+
+	class TestLayer : public Layer
+	{
+	private:
+		std::string m_Name;
+	public:
+		TestLayer(std::string&& name)
+			: m_Name(std::move(name))
+		{
+		}
+
+		virtual void OnUpdate() override {}
+		virtual bool OnEvent(Event& event) override
+		{
+			switch (event.GetType())
+			{
+			case EventType::KeyReleased:
+				std::cout << m_Name << ": Key released\n";
+				break;
+			}
+
+			return true;
+		}
+
+		virtual void OnAttach() override {}
+		virtual void OnDetach() override {}
+	};
+} // namespace sge
