8b52d9a820c4a2db2ea4ac81a03aaf04be3cc18f
unknown <davembertels@gmail.com>
2022-05-13 18:23:57 -0400

    Added glfw event callbacks.

--- engine/src/Window.cpp
+++ engine/src/Window.cpp
@@ -1,31 +1,68 @@
 #include "Window.h"
 #include "base.h"
-#include "vulkan/Instance.h"
+#include "event/KeyEvent.h"
 
 namespace sge
 {
 	Window::Window()
-		: m_WindowHandle(nullptr)
 	{
 		SGE_ASSERTM(glfwInit(), "Failed to initialize glfw.");
 
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 		constexpr uint32_t windowWidth = 800, windowHeight = 600;
-		m_WindowHandle = glfwCreateWindow(windowWidth, windowHeight, "Sigma Game Engine", nullptr, nullptr);
-
-		m_VulkanInstance = std::make_unique<vulkan::Instance>(m_WindowHandle);
+		GLFWwindow* windowHandle = glfwCreateWindow(windowWidth, windowHeight, "Sigma Game Engine", nullptr, nullptr);
+		std::cout << "Initializing Vulkan...\n";
+		m_VulkanInstance = std::make_unique<vulkan::Instance>(windowHandle);
+		
+		glfwSetWindowUserPointer(windowHandle, this);
+		InitEventCallbacks();
 	}
 
 	Window::~Window()
 	{
-		Destroy();
+		glfwDestroyWindow(m_VulkanInstance->GetWindowHandle());
+		glfwTerminate();
 	}
 
-	void Window::Destroy() const
+	void Window::InitEventCallbacks()
 	{
-		glfwDestroyWindow(m_WindowHandle);
-		glfwTerminate();
+		glfwSetKeyCallback(m_VulkanInstance->GetWindowHandle(),
+		[](GLFWwindow* windowHandle, int key, int scancode, int action, int mods)
+		{
+			Window* window = reinterpret_cast<Window*>(glfwGetWindowUserPointer(windowHandle));
+			if (action == GLFW_PRESS || action == GLFW_REPEAT)
+			{
+				KeyPressedEvent event(key);
+				window->OnEvent(event);
+			}
+			else
+			{
+				KeyReleasedEvent event(key);
+				window->OnEvent(event);
+			}
+		});
+
+		glfwSetWindowCloseCallback(m_VulkanInstance->GetWindowHandle(),
+		[](GLFWwindow* windowHandle)
+		{
+			Window* window = reinterpret_cast<Window*>(glfwGetWindowUserPointer(windowHandle));
+			// Create window close event
+			// ...
+		}
+		);
+
+		glfwSetFramebufferSizeCallback(m_VulkanInstance->GetWindowHandle(),
+		[](GLFWwindow* windowHandle, int width, int height)
+		{
+			Window* window = reinterpret_cast<Window*>(glfwGetWindowUserPointer(windowHandle));
+			window->OnFrameBufferResize();
+		});
+	}
+
+	void Window::OnEvent(Event& event)
+	{
+		m_Eventcallback(event);
 	}
 
 	void Window::OnUpdate()
@@ -34,4 +71,8 @@ namespace sge
 		glfwPollEvents();
 	}
 
+	void Window::OnFrameBufferResize()
+	{
+		m_VulkanInstance->SetFramebufferResized();
+	}
 } // namespace sge
--- engine/src/Window.h
+++ engine/src/Window.h
@@ -1,29 +1,30 @@
 #pragma once
 
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include "vulkan/Instance.h"
+#include "event/Event.h"
 
 #include <memory>
+#include <functional>
 
 namespace sge
 {
-	namespace vulkan
-	{
-		class Instance;
-	}
+	using EventCallback = std::function<void(Event&)>;
 	
 	class Window
 	{
 	private:
-		GLFWwindow* m_WindowHandle;
 		std::unique_ptr<vulkan::Instance> m_VulkanInstance;
+		EventCallback m_Eventcallback;
+	private:
+		void InitEventCallbacks();
 	public:
 		Window();
 		~Window();
-		void Destroy() const;
 		void OnUpdate();
+		void OnEvent(Event& event);
+		void OnFrameBufferResize();
 
-		inline GLFWwindow* GetWindowHandle() const { return m_WindowHandle; }
-		inline bool ShouldClose() const { return glfwWindowShouldClose(m_WindowHandle); }
+		inline bool ShouldClose() const { return glfwWindowShouldClose(m_VulkanInstance->GetWindowHandle()); }
+		inline void SetEventCallback(const EventCallback& callback) { m_Eventcallback = callback; }
 	};
 } // namespace sge
