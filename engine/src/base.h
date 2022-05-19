7f0e8b3bfa6358a51747e99200333a88cc7c9773
unknown <davembertels@gmail.com>
2022-05-19 13:55:05 -0400

    Added macros for compiling as shared or static library.

--- engine/src/base.h
+++ engine/src/base.h
@@ -10,3 +10,15 @@
 	#define SGE_ASSERTM(expr, msg) if (!(expr)) SGE_DEBUG_BREAKM(msg)
 	#define SGE_ASSERTF(expr, format, ...) if (!(expr)) { fprintf(stderr, "Assertion Failed: "format"\n", __VA_ARGS__); SGE_DEBUG_BREAK(); }
 #endif // DEBUG
+
+#define SGE_BUILD_SHARED
+// Maybe use this macro in the future to allow both static and shared library
+#ifdef SGE_BUILD_SHARED
+	#ifdef SGE_DLL_EXPORT
+		#define SGE_API _declspec(dllexport)
+	#else
+		#define SGE_API _declspec(dllimport)
+	#endif
+#else
+	#define SGE_API
+#endif // SGE_BUILD_SHARED
